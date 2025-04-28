950aabad9fae23150cd9b041db15db89bd78a008
Reinaldo Ferreira <reinaldo.mferreira@yahoo.com.br>
2025-04-28 17:14:31 -0300

    Correção de tipagem do Theme no styled-components

--- src/types/theme.d.ts
+++ src/types/theme.d.ts
@@ -1,49 +1,49 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 // src/styles/types/theme.d.ts
 
-import 'styled-components';  // Importando para estender a tipagem do styled-components
+import 'styled-components' // Importando para estender a tipagem do styled-components
 
 export interface Theme {
-  appBackground: string;
-  appColor: string;
-  appDefaultStroke: string;
-  appLogo: string;
-  appSkeletonFrom: string;
-  appSkeletonTo: string;
+  appBackground: string
+  appColor: string
+  appDefaultStroke: string
+  appLogo: string
+  appSkeletonFrom: string
+  appSkeletonTo: string
   buttons: {
-    alert: string;
-    alertColor: string;
-    alertHover: string;
-    disabled: string;
-    disabledColor: string;
-    primary: string;
-    primaryColor: string;
-    primaryHover: string;
-  };
+    alert: string
+    alertColor: string
+    alertHover: string
+    disabled: string
+    disabledColor: string
+    primary: string
+    primaryColor: string
+    primaryHover: string
+  }
   card: {
-    alert: string;
-    background: string;
-    border: string;
-    success: string;
-    warning: string;
-  };
+    alert: string
+    background: string
+    border: string
+    success: string
+    warning: string
+  }
   textInput: {
-    active: string;
-    activeColor: string;
-    borderColor: string;
-    disabled: string;
-    disabledBorderColor: string;
-    disabledColor: string;
-    placeholderColor: string;
-  };
+    active: string
+    activeColor: string
+    borderColor: string
+    disabled: string
+    disabledBorderColor: string
+    disabledColor: string
+    placeholderColor: string
+  }
   typographies: {
-    error: string;
-    subtitle: string;
-    success: string;
-  };
+    error: string
+    subtitle: string
+    success: string
+  }
 }
 
 // Estende o DefaultTheme do styled-components para usar a interface Theme
 declare module 'styled-components' {
-  export interface DefaultTheme extends Theme { }
+  export interface DefaultTheme extends Theme {}
 }
